d88d60ff1e7b868fb5d6fc19c10689346a5e40a2
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-09-10 22:56:10 +0300

    Context Class Created.

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-9NPFMUJ;database=CoreBlogDb;integrated security=true;");
+        }
+    }
+}
