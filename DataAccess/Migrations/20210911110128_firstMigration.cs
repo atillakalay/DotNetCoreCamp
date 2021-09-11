23fefb3cbf3f9b3e48380a58312d0486a76f77b9
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-09-11 14:02:23 +0300

    Migration created.

---
+++ DataAccess/Migrations/20210911110128_firstMigration.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Abouts",
+                columns: table => new
+                {
+                    AboutId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AboutDetails1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AboutDetails2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AboutImage1 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AboutImage2 = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AboutMapLocation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AboutStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Abouts", x => x.AboutId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Blogs",
+                columns: table => new
+                {
+                    BlogId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BlogTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BlogContent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BlogThumbNailImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BlogImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BlogCreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BlogStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Blogs", x => x.BlogId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CategoryName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CategoryStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    CommentId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CommentUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommentTitle = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommentContent = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommentDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CommentStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.CommentId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactUserName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactMail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactSubject = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactMessage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ContactStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Writers",
+                columns: table => new
+                {
+                    WriterId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WriterName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WriterAbout = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WriterImage = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WriterMail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WriterPassword = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    WriterStatus = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Writers", x => x.WriterId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Abouts");
+
+            migrationBuilder.DropTable(
+                name: "Blogs");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Writers");
+        }
+    }
+}
