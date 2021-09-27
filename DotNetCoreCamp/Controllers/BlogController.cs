5c3868da8a76ff275f04561bd4bc70f07676070d
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-09-27 19:48:24 +0300

    22. Ders tamamlandı.

---
+++ DotNetCoreCamp/Controllers/BlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreCamp.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
