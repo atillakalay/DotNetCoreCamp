2b36bfcef4a05a075d9cde5adffeeea8f7143def
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-10-09 13:58:40 +0300

    Ödevler Tamamlandı.

--- DotNetCoreCamp/Controllers/RegisterController.cs
+++ DotNetCoreCamp/Controllers/RegisterController.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Business.Concrete;
 using Business.ValidationRules;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
 using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DotNetCoreCamp.Controllers
 {
@@ -14,6 +18,7 @@ namespace DotNetCoreCamp.Controllers
         [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Cities = GetCityList();
             return View();
         }
         [HttpPost]
@@ -40,6 +45,21 @@ namespace DotNetCoreCamp.Controllers
                 ModelState.AddModelError("WriterPassword", "Girdiğiniz Şifreler Eşleşmedi Lütfen Tekrar Deneyin");
             }
             return View();
+        }
+        public List<SelectListItem> GetCityList()
+        {
+            List<SelectListItem> adminRole = (from x in GetCity()
+                select new SelectListItem
+                {
+                    Text = x,
+                    Value = x
+                }).ToList();
+            return adminRole;
+        }
+        public List<string> GetCity()
+        {
+            String[] citiesArray = new String[] { "Adana", "Adıyaman", "Afyon", "Ağrı", "Aksaray", "Amasya", "Ankara", "Antalya", "Ardahan", "Artvin", "Aydın", "Bartın", "Batman", "Balıkesir", "Bayburt", "Bilecik", "Bingöl", "Bitlis", "Bolu", "Burdur", "Bursa", "Çanakkale", "Çankırı", "Çorum", "Denizli", "Diyarbakır", "Düzce", "Edirne", "Elazığ", "Erzincan", "Erzurum", "Eskişehir", "Gaziantep", "Giresun", "Gümüşhane", "Hakkari", "Hatay", "Iğdır", "Isparta", "İçel", "İstanbul", "İzmir", "Karabük", "Karaman", "Kars", "Kastamonu", "Kayseri", "Kırıkkale", "Kırklareli", "Kırşehir", "Kilis", "Kocaeli", "Konya", "Kütahya", "Malatya", "Manisa", "Kahramanmaraş", "Mardin", "Muğla", "Muş", "Nevşehir", "Niğde", "Ordu", "Osmaniye", "Rize", "Sakarya", "Samsun", "Siirt", "Sinop", "Sivas", "Tekirdağ", "Tokat", "Trabzon", "Tunceli", "Şanlıurfa", "Şırnak", "Uşak", "Van", "Yalova", "Yozgat", "Zonguldak" };
+            return new List<string>(citiesArray);
         }
     }
 }
---
+++ DotNetCoreCamp/Models/CityAndWriter.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Entities.Concrete;
+
+namespace DotNetCoreCamp.Models
+{
+    public class CityAndWriter
+    {
+        public List<Cities> Cities { get; set; }
+        public List<Writer> Writers { get; set; }
+    }
+}
