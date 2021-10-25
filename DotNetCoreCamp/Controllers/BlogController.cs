c4910fb35356f6b43e49cf7e5e9bac83b85e4e28
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-10-25 13:44:22 +0300

    54.Ders Tamamlandı.

--- DotNetCoreCamp/Controllers/BlogController.cs
+++ DotNetCoreCamp/Controllers/BlogController.cs
@@ -9,17 +9,25 @@ namespace DotNetCoreCamp.Controllers
     public class BlogController : Controller
     {
         private BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+        [HttpPost]
         public IActionResult Index()
         {
             var blogs = _blogManager.GetBlogListWithCategory();
             return View(blogs);
         }
 
+        [HttpGet]
         public IActionResult BlogReadAll(int id)
         {
             ViewBag.blogId = id;
             var blog = _blogManager.GetAll(id);
             return View(blog);
+        }
+
+        public IActionResult BlogListByWriter()
+        {
+            var blogs = _blogManager.GetBlogListWithWriter(1);
+            return View(blogs);
         }
     }
 }
