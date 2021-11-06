b863ffaf0f1a6f8c71c0c0ec572b25309211a45d
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-11-06 13:42:27 +0300

    72.Ders Tamamlandı.

--- DotNetCoreCamp/Controllers/WriterController.cs
+++ DotNetCoreCamp/Controllers/WriterController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using Business.ValidationRules;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using FluentValidation.Results;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
 namespace DotNetCoreCamp.Controllers
@@ -6,6 +11,8 @@ namespace DotNetCoreCamp.Controllers
 
     public class WriterController : Controller
     {
+        private WriterManager _writerManager = new WriterManager(new EfWriterRepository());
+
         [Authorize]
         public IActionResult Index()
         {
@@ -27,6 +34,39 @@ namespace DotNetCoreCamp.Controllers
         public PartialViewResult WriterFooterPartial()
         {
             return PartialView();
+        }
+
+        [AllowAnonymous]
+        [HttpGet]
+        public IActionResult WriterEditProfile()
+        {
+            var writerValues = _writerManager.GetById(1);
+            return View(writerValues);
+        }
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult WriterEditProfile(Writer writer, string passwordAgain)
+        {
+            WriterValidator writerValidator = new WriterValidator();
+            ValidationResult results = writerValidator.Validate(writer);
+            if (results.IsValid && writer.WriterPassword == passwordAgain)
+            {
+                writer.WriterStatus = true;
+                _writerManager.Update(writer);
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else if (!results.IsValid)
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("WriterPassword", "Girdiğiniz Şifreler Eşleşmiyor! Lütfen Tekrar Deneyiniz");
+            }
+            return View();
         }
     }
 }
