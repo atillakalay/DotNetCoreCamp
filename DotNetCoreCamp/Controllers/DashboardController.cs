88a02218090a247e7c97491e217aacd85c76d66c
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-11-06 13:17:55 +0300

    70.Ders Tamamlandı.

--- DotNetCoreCamp/Controllers/DashboardController.cs
+++ DotNetCoreCamp/Controllers/DashboardController.cs
@@ -1,13 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
 
 namespace DotNetCoreCamp.Controllers
 {
     public class DashboardController : Controller
     {
+        private BlogManager _blogManager = new BlogManager(new EfBlogRepository());
+        private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
         [AllowAnonymous]
         public IActionResult Index()
         {
+            ViewBag.blogsCount = _blogManager.GetAll().Count;
+            ViewBag.blogCountByWriter = _blogManager.GetBlogListWithWriter(2).Count;
+            ViewBag.categoriesCount = _categoryManager.GetAll().Count;
             return View();
         }
     }
