723503559e9cfa8d7d4f72663db148cd1636be6b
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-11-30 11:26:38 +0300

    80.Ders Tamamlandı.

--- DotNetCoreCamp/Controllers/NotificationController.cs
+++ DotNetCoreCamp/Controllers/NotificationController.cs
@@ -1,16 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DotNetCoreCamp.Controllers
 {
     public class NotificationController : Controller
     {
+        private NotificationManager _notificationManager = new NotificationManager(new EfNotificationRepository());
         public IActionResult Index()
         {
             return View();
+        }
+        [AllowAnonymous]
+        public IActionResult AllNotification()
+        {
+            var notifications = _notificationManager.GetAll();
+            return View(notifications);
         }
     }
 }
