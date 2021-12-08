989929b232ac83b0049e038edda88fe59a9aa8d2
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-12-08 14:05:01 +0300

    90.Ders Tamamlandı.

--- DotNetCoreCamp/Startup.cs
+++ DotNetCoreCamp/Startup.cs
@@ -69,6 +69,10 @@ namespace DotNetCoreCamp
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
