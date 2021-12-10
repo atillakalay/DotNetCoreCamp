83c9a451007a7961fe2b1b944b1b79313b76a954
Atilla <63123956+atillakalay@users.noreply.github.com>
2021-12-10 23:24:28 +0300

    91. Ders Tamamlandı.

--- DotNetCoreCamp/Areas/Admin/Controllers/CategoryController.cs
+++ DotNetCoreCamp/Areas/Admin/Controllers/CategoryController.cs
@@ -3,15 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
 
 namespace DotNetCoreCamp.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class CategoryController : Controller
     {
+        private CategoryManager _categoryManager = new CategoryManager(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            var categories = _categoryManager.GetAll();
+            return View(categories);
         }
     }
 }
