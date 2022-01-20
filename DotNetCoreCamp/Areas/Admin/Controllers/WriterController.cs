2bb1e14b86e823d547779ce5a2da4b9919e01787
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-20 15:37:57 +0300

    113. Ders Tamamlandı.

--- DotNetCoreCamp/Areas/Admin/Controllers/WriterController.cs
+++ DotNetCoreCamp/Areas/Admin/Controllers/WriterController.cs
@@ -11,17 +11,6 @@ namespace DotNetCoreCamp.Areas.Admin.Controllers
     [Area("Admin")]
     public class WriterController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult WriterList()
-        {
-            var jsonWriters = JsonConvert.SerializeObject(writers);
-            return Json(jsonWriters);
-        }
-
         public static List<WriterClass> writers = new List<WriterClass>
         {
             new WriterClass
@@ -40,5 +29,52 @@ namespace DotNetCoreCamp.Areas.Admin.Controllers
                 Name = "Talha"
             },
         };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult WriterList()
+        {
+            var jsonWriters = JsonConvert.SerializeObject(writers);
+            return Json(jsonWriters);
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var findWriter = writers.FirstOrDefault(w => w.Id == id);
+            var jsonWriter = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriter);
+        }
+        public IActionResult GetWriterByID(int writerid)
+        {
+            var findWriter = writers.FirstOrDefault(x => x.Id == writerid);
+            var jsonWriters = JsonConvert.SerializeObject(findWriter);
+            return Json(jsonWriters);
+        }
+
+        [HttpPost]
+        public IActionResult AddWriter(WriterClass writerClass)
+        {
+            writers.Add(writerClass);
+            var jsonWriters = JsonConvert.SerializeObject(writerClass);
+            return Json(jsonWriters);
+
+        }
+        public IActionResult DeleteWriter(int id)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == id);
+            writers.Remove(writer);
+            return Json(writer);
+        }
+        public IActionResult UpdateWriter(WriterClass writerClass)
+        {
+            var writer = writers.FirstOrDefault(x => x.Id == writerClass.Id);
+            writer.Name = writerClass.Name;
+            var jsonWriter = JsonConvert.SerializeObject(writerClass);
+            return Json(jsonWriter);
+        }
+
+
     }
 }
