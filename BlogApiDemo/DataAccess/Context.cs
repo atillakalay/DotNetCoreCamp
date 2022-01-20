eb1c7498741ce38017d17849be67a86c9a140d00
Atilla <63123956+atillakalay@users.noreply.github.com>
2022-01-20 16:08:09 +0300

    114.Ders Tamamlandı.

---
+++ BlogApiDemo/DataAccess/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApiDemo.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogApiDemo.DataAccess
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-9NPFMUJ;database=CoreBlogApiDb;integrated security=true;");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ BlogApiDemo/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlogApiDemo.Entities
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
